a87ab9c00519ecda8044a334c050a7504ab19ca1
kishan <kishanvaghani40@gmail.com>
2021-05-26 17:05:53 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { style } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kishanPortfolio';
+  scrolldown = true;
+  toogle = false;
+  collapsing=false;
+
+  toogleBtn() {
+    this.toogle = !this.toogle;
+  }
+
+  img=[
+    {
+      id:1,
+      src:"../assets/uploads/gallery_img-01.jpg"
+    },
+    {
+      id:2,
+      src:"../assets/uploads/gallery_img-02.jpg"
+    },
+    {
+      id:3,
+      src:"../assets/uploads/gallery_img-03.jpg"
+    },
+    {
+      id:4,
+      src:"../assets/uploads/gallery_img-04.jpg"
+    },
+    {
+      id:5,
+      src:"../assets/uploads/gallery_img-05.jpg"
+    },
+    {
+      id:6,
+      src:"../assets/uploads/gallery_img-06.jpg"
+    }
+  ]
 }
